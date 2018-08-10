c888c63da759f9a0c53134278cf3085c7d466881
Michael Cao <mcc249@cornell.edu>
2018-08-10 14:49:41 -0400

    Update LaunchPADGUI.sh
    Shell script to launch camserver and PADGUI

--- LaunchPADGUI.sh
+++ LaunchPADGUI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gnome-terminal --working-directory=/home/empad/tvx_64/tvx/camera/camserver -e ./camserver
-cd /home/empad/Desktop/empadgui_cu
+cd /home/empad/Desktop/empadgui_cu-master
 python empadgui_cu.py
